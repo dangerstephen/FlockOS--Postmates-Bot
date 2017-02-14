c6deb412b45988d357571c326a3b113d34473a71
dangerstephen <dangersteve1@gmail.com>
2017-02-14 11:28:37 -0800

    final commit for the devweek hackathon app 2 of 2

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appId: '546d1aaf-1afe-481f-a043-518e6647659d',
+    appSecret: 'b63b270a-9d2f-4de3-ae3e-97de1a7f0e5b',
+    botUserId: 'u:B6ll6adwd3l6w4a9',
+    botToken:  'df2212f1-52d5-4288-b1a0-0bc8ba73622d',
+    endpoint: 'https://38efdcd8.ngrok.io'
+};
